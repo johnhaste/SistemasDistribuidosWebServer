66e3059d3de53fce29a61de5884b24768ae5eb32
John Haste <joaovictorspteixeira@gmail.com>
2018-06-09 19:20:10 -0300

    Initial Commit

---
+++ Web/Controllers/LivroController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class LivroController : ApiController
+    {
+        //Livros dummys para teste
+        Livro[] baseDeLivros = new Livro().RetornaLivros();
+
+        /*public IEnumerable<Livro> GetAllLivros()
+        {
+            return baseDeLivros;
+        }*/
+
+        public JsonResult GetAllLivros()
+        {
+            return baseDeLivros;
+        }
+
+        public Livro GetLivroById(int id)
+        {
+            var livro = baseDeLivros.FirstOrDefault((p) => p.Id == id);
+            if (livro == null)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.NotFound);
+                throw new HttpResponseException(resp);
+            }
+            return livro;
+        }
+
+        public IEnumerable<Livro> GetLivrosByCategory(string categoria)
+        {
+            return baseDeLivros.Where(
+                (p) => string.Equals(p.Categoria, categoria,
+                    StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ Web/Models/Livro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Livro
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+        public Livro[] livros;
+
+
+        public Livro[] RetornaLivros() {
+            livros = new Livro[]
+            {
+                new Livro { Id = 1, Nome = "Livro 123", Categoria = "Sci-Fi", Preco= 45.60M },
+                new Livro { Id = 2, Nome = "Livro 456", Categoria = "Drama", Preco = 23.75M },
+                new Livro { Id = 3, Nome = "Livro 789", Categoria = "Ação", Preco = 19.90M }
+            };
+            return livros;
+        }
+
+
+    }
+}
